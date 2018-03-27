3bf79bc7cc7690956bb76b32cc59621b6b3296e0
Ian Spielman <ian.spielman@nist.gov>
2018-03-27 10:18:05 -0400

    Added RbLi changes.

--- NewFocusPicoMotorController_8742.py
+++ NewFocusPicoMotorController_8742.py
@@ -20,23 +20,21 @@ class NewFocus8742Motor(StaticAnalogQuantity):
     minval=-2147483648
     maxval=2147483647
     description = 'PicoMotor'
-    
+
 @labscript_device
 class NewFocusPicoMotorController_8742(Device):
     allowed_children = [NewFocus8742Motor]
     generation = 0
-    
-    @set_passed_properties(property_names = {
-        "connection_table_properties":["host", "port"]}
-        )
-    def __init__(self, name, 
-                 host="",
-                 port=23, 
-                 **kwargs):
-            
-        Device.__init__(self, name, None, None, None, **kwargs)
-        self.BLACS_connection = '%s' %(host)
-        
+
+    @set_passed_properties()
+
+    def __init__(self, name, host = "", port=23, slave=1, **kwargs):
+        # slave=1 is the master controller on the host ip
+
+        Device.__init__(self, name, None, None, None)
+        self.BLACS_connection = '%s,%d' %(host, slave)
+
+
     def generate_code(self, hdf5_file):
         data_dict = {}
         for motor in self.child_devices:
@@ -56,142 +54,180 @@ class NewFocusPicoMotorController_8742(Device):
         dtypes = [(conn, int) for conn in data_dict]
         data_array = np.zeros(1, dtype=dtypes)
         for conn in data_dict:
-            data_array[0][conn] = data_dict[conn] 
+            data_array[0][conn] = data_dict[conn]
         grp = hdf5_file.create_group('/devices/'+self.name)
         grp.create_dataset('static_values', data=data_array)
-        
+
 import time
 
 from blacs.tab_base_classes import Worker, define_state
-from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED  
+from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED
 
 from blacs.device_base_class import DeviceTab
 
 @BLACS_tab
 class NewFocusPicoMotorControllerTab(DeviceTab):
     def initialise_GUI(self):
-        
         # Capabilities
         self.base_units = 'steps'
         self.base_min = -2147483648
         self.base_step = 10
         self.base_decimals = 0
-        
-        device = self.settings['connection_table'].find_by_name(self.device_name)
-        
+
+        self.device = self.settings['connection_table'].find_by_name(self.device_name)
+        self.num_motors = len(self.device.child_list)
+
         # Create the AO output objects
-        self.connections = [device.child_list[child_name].parent_port for child_name in device.child_list]
-        
         ao_prop = {}
-        for child_name in device.child_list:
-            motor_type = device.child_list[child_name].device_class
-            connection = device.child_list[child_name].parent_port
+        for child_name in self.device.child_list:
+            motor_type = self.device.child_list[child_name].device_class
+            connection = self.device.child_list[child_name].parent_port
             if motor_type == "NewFocus8742Motor":
                 base_max = 2147483647
             else:
                 base_max = 2147483647
-            
+
             ao_prop[connection] = {'base_unit':self.base_units,
                                    'min':self.base_min,
                                    'max':base_max,
                                    'step':self.base_step,
                                    'decimals':self.base_decimals
                                   }
-                                
-        # Create the output objects    
+
+        # Create the output objects
         self.create_analog_outputs(ao_prop)
-        
         # Create widgets for output objects
         dds_widgets,ao_widgets,do_widgets = self.auto_create_widgets()
         # and auto place the widgets in the UI
-        self.auto_place_widgets(("Target Position", ao_widgets))
-        
+        self.auto_place_widgets(("Target Position",ao_widgets))
+
         # Store the address
-        self.host = str(device.properties['host'])
-        
+        self.blacs_connection = str(self.settings['connection_table'].find_by_name(self.device_name).BLACS_connection)
+        self.host, self.slave = self.blacs_connection.split(',')
+        # print(self.host.split())
+
         # Set the capabilities of this device
-        self.supports_remote_value_check(False)
-        self.supports_smart_programming(False) 
-    
+        self.supports_remote_value_check(True)
+        self.supports_smart_programming(False)
+
     def initialise_workers(self):
         # Create and set the primary worker
-        self.create_worker("main_worker", NewFocusPicoMotorControllerWorker, {'host':self.host, 'connections': self.connections})
+        self.create_worker("main_worker",NewFocusPicoMotorControllerWorker,{'host':self.host, 'slave':self.slave})
         self.primary_worker = "main_worker"
 
-@BLACS_worker    
+@BLACS_worker
 class NewFocusPicoMotorControllerWorker(Worker):
     def init(self):
         global socket; import socket
         global zprocess; import zprocess
         global h5py; import labscript_utils.h5_lock, h5py
         global time; import time
-        
-        # The current position of the motor is unknown.  Use the first set
-        self.current_values = {connection:None for connection in self.connections}     
-        
-        self.port = 23
-        
-    def check_connectivity(self, host):
-        try:
-            response = self.initialise_sockets(self.host, self.port)
-        except:
-            raise Exception("sockets are not initialised")
-        print response
-        if '8742' in response:
-            return True
-        else:
-            raise Exception('invalid response from host: ' + str(response))
-        
-    def initialise_sockets(self, host, port):
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        assert port, 'No port number supplied.'
-        assert host, 'No hostname supplied.'
-        # s.settimeout(5)
-        # s.connect((host, int(port)))
-        time.sleep(0.1)
-        # s.send("*IDN?"+'\n')
-        time.sleep(0.1)
-        # response = s.recv(1024)
-        time.sleep(0.005)
-        # s.close()
-        # return response
-        return '8742'
-        
-    def program_manual(self, front_panel_values):
-        try:
-            self.check_connectivity(self.host)
-            # For each motor
-            # TODO: are we really assuming that there are four axes?
-            for axis in range(1,5):
-                self.program_static(axis, int(front_panel_values["%s" %axis]))
-                time.sleep(0.01)
-            return {}
-        except:
-            raise Exception("server not responding, check connectivity failed")
 
-    
-    def program_static(self, axis, value):
-        # Target Position ONLY
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        time.sleep(0.1)
+        self.port = 23
+        self.prefix = '%s>' % self.slave
+
+        # TODO change connectivity to establish connection only once
+        # rather than every time I want to send a command
+
+    def readline(self, socket):
+        data = ''
+        while True:
+            char = socket.recv(1)
+            data += char
+            if char == '\n':
+                return data
+
+
+    def check_remote_values(self):
+        # Get the currently output values:
+        results = {}
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # time.sleep(0.1)
         assert self.port, 'No port number supplied.'
         assert self.host, 'No hostname supplied.'
+        s.settimeout(5)
+        s.connect((self.host, int(self.port)))
+
+        for axis in range(1,5):
+            command = self.prefix+"xxPA?"
+            full_command = command.replace("xx", str(axis))
+            # full_command = axis_command.replace("nn", str(value))
+            # print full_command
+
+            # retry 5 times to send the command
+            for _ in range(5):
+                try:
+                    s.send(full_command +'\n')
+                    # time.sleep(0.1)
+                    # response = s.recv(1024)
+                    response = self.readline(s)
+                    # skip hex identifier in response
+                    # print(self.host + ' init: ' + response)
+                    # time.sleep(0.1)
+                    results['%s'%axis] = int(response.split('>')[1].strip())
+                    break
+                except Exception as e:
+                    print(e)
+                    print('Retrying remote value check.')
+
+        return results
+
+    def check_connectivity(self, host):
+        s = self.initialise_sockets(self.host, self.port)
+
+        s.send(self.prefix+"*IDN?"+'\n')
+        response = self.readline(s)
+
+        # print response
+        if '8742' in response:
+            return s
+        else:
+            raise Exception('invalid response from host: ' + str(response))
+
+    def initialise_sockets(self, host, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        assert port, 'No port number supplied.'
+        assert host, 'No hostname supplied.'
+        s.settimeout(5)
+        s.connect((host, int(port)))
+        # time.sleep(0.1)
+        # s.send(self.prefix+"*IDN?"+'\n')
+        # time.sleep(0.1)
+        # response = s.recv(1024)
+        # time.sleep(0.005)
+        # s.close()
+        return s
+
+    def program_manual(self, front_panel_values):
+        s = self.check_connectivity(self.host)
+        # For each motor
+        for axis in range(1,5):
+            self.program_static(s, axis, int(front_panel_values["%s" %axis]))
+            # time.sleep(0.01)
+
+        s.close()
+        # return {}
+        return self.check_remote_values()
+
+
+    def program_static(self, socket, axis, value):
+        # Target Position ONLY
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # time.sleep(0.1)
+        # assert self.port, 'No port number supplied.'
+        # assert self.host, 'No hostname supplied.'
         # s.settimeout(5)
         # s.connect((self.host, int(self.port)))
-        
-        # TODO: fix string syntax
-        command = "xxPAnn"
+        command = self.prefix+"xxPAnn"
         axis_command = command.replace("xx", str(axis))
         full_command = axis_command.replace("nn", str(value))
-        print full_command
-        
-        # TODO: Remove print statements since they are leftover debug code
-        # s.send(full_command +'\n')
-        time.sleep(0.1)
-        # print s.recv(1024)
-        time.sleep(0.005)
+        # print full_command
+        socket.send(full_command +'\n')
+        # time.sleep(0.1)
+        # s.recv(1024)  # clears buffer
+        # time.sleep(0.005)
         # s.close()
-    
+
     def transition_to_buffered(self,device_name,h5file,initial_values,fresh):
         return_data = {}
         with h5py.File(h5file) as hdf5_file:
@@ -199,22 +235,21 @@ class NewFocusPicoMotorControllerWorker(Worker):
             if 'static_values' in group:
                 data = group['static_values'][:][0]
         self.program_manual(data)
-        
+
         for motor in data.dtype.names:
             return_data[motor] = data[motor]
 
         return return_data
-    
+
     def transition_to_manual(self):
         #self.program_manual(self.initial_values)
         return True
-    
+
     def abort_buffered(self):
         return True
-        
+
     def abort_transition_to_buffered(self):
         return True
-    
+
     def shutdown(self):
         self.s.close()
-            
